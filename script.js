ff5708bf366cf1f1bf7467cd1fc145e04452bc99
Shivanshu Khanna <61427236+Shivanshukhanna@users.noreply.github.com>
2020-09-01 12:52:35 +0530

    adding script to js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+class healthbar {
+  constructor(element,initialValue=0) {
+    this.valueElem= element.querySelector('.health-bar-value');
+    this.fillElem=element.querySelector('.health-bar-fill');
+
+    this.setValue(initialValue);
+  }
+    setValue(newValue){
+      this.value=newValue;
+      this.update();
+    }
+    update(){
+      const percentage= this.value +'%';
+
+      this.fillElem.style.width=percentage;
+      this.valueElem.textContent=percentage;
+    }
+}
+
+ const pb1=new healthbar(document.querySelector('.health-bar'),100);
